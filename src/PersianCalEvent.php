cdfd2f94dac4ecdceacf13bf39580885590872ec
S.Ahmad Mousavi <mousavi7731@gmail.com>
2023-02-01 22:59:04 +0330

    Init

---
+++ src/PersianCalEvent.php
@@ -0,0 +1,70 @@
+<?php namespace seyyedam7\PersianCalEvent;
+
+use Carbon\Carbon;
+
+/**
+*
+*  @author Seyyed Ahmad Mousavi
+*/
+class PersianCalEvent
+{
+
+    /**
+     * @param int $y
+     * @param int $m
+     * @param int $d
+     * @param $type
+     * @return \stdClass
+     */
+    private static function crawl($y, $m, $d, $type)
+    {
+        $handle = curl_init();
+
+        $url = "https://persiancalapi.ir/".$type."/".$y."/".$m."/".$d;
+
+        curl_setopt($handle, CURLOPT_URL, $url);
+        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handle, CURLOPT_FAILONERROR, true);
+        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+
+        $output = curl_exec($handle);
+
+        if (curl_errno($handle)) {
+            print curl_error($handle);
+        }
+
+        curl_close($handle);
+
+        $output = json_decode($output);
+
+        return $output;
+    }
+
+    /**
+     * @param int $y
+     * @param int $m
+     * @param int $d
+     * @return \stdClass
+     */
+    public static function jalali($y, $m, $d)
+    {
+        return self::crawl($y, $m ,$d, 'jalali');
+    }
+
+    /**
+     * @param int|Carbon $y
+     * @param int|null $m
+     * @param int|null $d
+     * @return \stdClass
+     */
+    public static function gregorian($y, $m = null, $d = null)
+    {
+        if ($m == null) {
+            $m = $y->month;
+            $d = $y->day;
+            $y = $y->year;
+        }
+
+        return self::crawl($y, $m, $d, 'gregorian');
+    }
+}
